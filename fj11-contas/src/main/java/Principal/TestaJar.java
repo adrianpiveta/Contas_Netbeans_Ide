585e45369e095a219dc85925a37975d3ffe7d038
adrianpiveta <danielpiveta257@gmail.com>
2021-11-03 23:30:24 -0300

    Tentativa de importar Jar

---
+++ fj11-contas/src/main/java/Principal/TestaJar.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Principal;
+
+/**
+ *
+ * @author danie
+ */
+import	javafx.api.main.OlaMundo;
+public class TestaJar	{
+public	static	void	main(String[]	args) {
+					OlaMundo.main(args);
+	}
+}
