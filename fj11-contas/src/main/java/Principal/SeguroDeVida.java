f82b4181664282da48ac56727abd898f4ed3236e
adrianpiveta <danielpiveta257@gmail.com>
2021-11-05 17:30:20 -0300

    Teste de abstração

---
+++ fj11-contas/src/main/java/Principal/SeguroDeVida.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Principal;
+
+/**
+ *
+ * @author danie
+ */
+public class SeguroDeVida {
+    double valor;
+    String titular;
+    int numeroApolice;
+    
+    public String getTipo(){
+        return "Seguro de vida";
+    }
+    
+}
---
+++ fj11-contas/src/main/java/Principal/TestaTributavel.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Principal;
+
+/**
+ *
+ * @author danie
+ */
+public class TestaTributavel {
+    public static void main(String[] args) {
+        Tributavel t1 = new ContaCorrente(333, "jose", 1, "22");
+        
+        System.out.println(t1);
+    }
+    
+}
